fda8e440990e7eb05563673caa3d8bb9b2d26730
stefano <sbaiardi@bsdsoftware.it>
2022-02-09 10:05:25 +0100

    tolte icone in contatti

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { faEnvelope,  } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  faCoffee = faEnvelope;
-  faFacebook = faFacebook;
-  faInstagram = faInstagram;
-  faLinkedin = faLinkedin;
-  faGithub = faGithub;
+
   constructor() { }
 
   ngOnInit(): void {
