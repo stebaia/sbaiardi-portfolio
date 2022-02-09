da289e3359425c539420ee9ddb7281945e9e548f
stefano <sbaiardi@bsdsoftware.it>
2022-02-09 08:35:34 +0100

    modifica testi

--- src/app/components/mock/mock-project.ts
+++ src/app/components/mock/mock-project.ts
@@ -4,14 +4,14 @@ export const PROJECTS: Project[] = [
     { 
         id: 1, 
         title: "Municipium", 
-        description: "Mi occupo dello sviluppo dell'applicazione Android, ho fatto diversi lavori tra cui manutenzione, refactoring del codice e restyling. Mi sono occupato anche dello sviluppo del backend e del backoffice.", 
+        description: "Attualmente, presso l'azienda per cui lavoro (BsdSoftware) mi occupo dello sviluppo di questa applicazione Android. Ho fatto diversi lavori tra cui manutenzione, refactoring del codice e restyling e mi sono occupato anche dello sviluppo del backend e del backoffice.", 
         abilities: "" ,
         image_path: "/assets/image/municipium.png", 
         background_color: "#131618" 
     },
-    { id: 2, title: "Vivaticket", description: "Ho lavorato per l'azienda Vivaticket. Per loro ho eseguito del refactoring, assistezna e restyling della loro app Ticket Manager.", abilities: "" ,image_path: "/assets/image/vivaticket.png", background_color: "#131618" },
-    { id: 3, title: "Bastonilaboratori", description: "Per l'azienda BastoniLaboratori ho sviluppato e attualmente sto manutenendo il sito web.", abilities: "" ,image_path: "/assets/image/bastonilab.png", background_color: "#131618" },
-    { id: 4, title: "Mayp", description: "Ho sviluppato", abilities: "" ,image_path: "/assets/image/vivaticket.png", background_color: "#131618" },
-    { id: 5, title: "Personal", description: "Ho creato svariati progetti sia applicazioni mobile che web, quasi tutti non pubblicati", abilities: "" ,image_path: "/assets/image/github.png", background_color: "#131618" },
+    { id: 2, title: "Vivaticket", description: "Sto lavorando da alcuni mesi per l'azienda Vivaticket. Per loro ho eseguito del refactoring, assistenza, restyling e implementazione di nuove funzionalità della loro app Ticket Manager.", abilities: "" ,image_path: "/assets/image/vivaticket.png", background_color: "#131618" },
+    { id: 3, title: "Bastonilaboratori", description: "Per l'azienda BastoniLaboratori, con cui collaboro da svariati anni, ho sviluppato e attualmente sto manutenendo il loro ecommerce. Mi sono inoltre occupato di gestire l'inserimento dei prodotti e creare un altro sito per la vendita degli stessi riservata ai commercianti.", abilities: "" ,image_path: "/assets/image/bastonilab.png", background_color: "#131618" },
+    { id: 4, title: "IdMatch", description: "Per MaypDigital, azienda con cui collaboro da alcuni mesi, ho sviluppato da zero e sto manuntendo un'applicazione Android, implementando grafiche e REST API fornite da loro.", abilities: "" ,image_path: "/assets/image/vivaticket.png", background_color: "#131618" },
+    { id: 5, title: "Personal", description: "Ho creato svariati progetti sia applicazioni mobile che web, quasi tutti non pubblicati. L'ultimo che invece è stato pubblicato è una App per Android che integra delle REST API usando il framework Flusk.", abilities: "" ,image_path: "/assets/image/github.png", background_color: "#131618" },
     
 ]
